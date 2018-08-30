11b4185d356f8e6b654ff2673718e6af3019b930
jakezp <jakezp@gmail.com>
2018-08-30 22:05:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,19 @@ if [[ ! -f /etc/nginx/conf.d/reverseproxy.conf ]]; then
   sed -i "s/HOSTIP/$HOSTIP/g" /etc/nginx/conf.d/reverseproxy.conf
   sed -i "s/DESTPORT/$DESTPORT/g" /etc/nginx/conf.d/reverseproxy.conf
 fi
+
 if [[ ! -f /etc/ssl/certs/nginx/dh2048.pem ]]; then
   openssl dhparam -out /etc/ssl/certs/nginx/dh2048.pem 2048
 fi
 
 # Use supervisord to start all processes
-supervisord -c /etc/supervisor/conf.d/supervisord.conf
+supervisord -c /etc/supervisor/conf.d/supervisord.conf &
+
+# Generate letsencrypt certificates
+mkdir -p /data/letsencrypt
+if [[ ! -d /etc/letsencrypt/live/$SERVERNAME ]]; then
+  echo -e "Generating certificates..."
+  certbot certonly --webroot --email $EMAIL --agree-tos --no-eff-email --webroot-path=/data/letsencrypt -d $SERVERNAME
+  sed -i 's/^## //g' /etc/nginx/conf.d/reverseproxy.conf
+  supervisorctl restart nginx
+fi
